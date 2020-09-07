b55b40ff4587b4d3b62dce8e3f3d3b8381e70052
Noah <59740042+noahjeffers@users.noreply.github.com>
2020-09-07 13:47:58 -0500

    formatted

--- src/App.js
+++ src/App.js
@@ -6,22 +6,22 @@ import Details from "./Details";
 import ThemeContext from "./ThemeContext";
 
 const App = () => {
-  const themeHook = useState("darkblue");
-  return (
-    <React.StrictMode>
-      <ThemeContext.Provider value={themeHook}>
-        <div>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <Router>
-            <SearchParams path="/" />
-            <Details path="/details/:id" />
-          </Router>
-        </div>
-      </ThemeContext.Provider>
-    </React.StrictMode>
-  );
+    const themeHook = useState("darkblue");
+    return (
+        <React.StrictMode>
+            <ThemeContext.Provider value={themeHook}>
+                <div>
+                    <header>
+                        <Link to="/">Adopt Me!</Link>
+                    </header>
+                    <Router>
+                        <SearchParams path="/" />
+                        <Details path="/details/:id" />
+                    </Router>
+                </div>
+            </ThemeContext.Provider>
+        </React.StrictMode>
+    );
 };
 
 // render(React.createElement(App), document.getElementById("root"));
--- src/Details.js
+++ src/Details.js
@@ -7,84 +7,86 @@ import ErrorBoundary from "./ErrorBoundary";
 import ThemeContext from "./ThemeContext";
 
 class Details extends React.Component {
-  // constructor(props) {
-  //   super(props);
+    // constructor(props) {
+    //   super(props);
 
-  //   this.state = {
-  //     loading: true,
-  //   };
-  // }
-  state = { loading: true, showModal: false };
-  componentDidMount() {
-    pet.animal(this.props.id).then(({ animal }) => {
-      this.setState({
-        url: animal.url,
-        name: animal.name,
-        animal: animal.type,
-        location: `${animal.contact.address.city}, ${animal.contact.address.address1.state}`,
-        description: animal.description,
-        media: animal.photos,
-        breed: animal.breeds.primary,
-        loading: false,
-      });
-      // eslint-disable-next-line no-console
-    }, console.error);
-  }
-
-  toggleModal = () => this.setState({ showModal: !this.state.showModal });
-  adopt = () => navigate(this.state.url);
-  render() {
-    if (this.state.loading) {
-      return <h1>loading ...</h1>;
+    //   this.state = {
+    //     loading: true,
+    //   };
+    // }
+    state = { loading: true, showModal: false };
+    componentDidMount() {
+        pet.animal(this.props.id).then(({ animal }) => {
+            this.setState({
+                url: animal.url,
+                name: animal.name,
+                animal: animal.type,
+                location: `${animal.contact.address.city}, ${animal.contact.address.address1.state}`,
+                description: animal.description,
+                media: animal.photos,
+                breed: animal.breeds.primary,
+                loading: false,
+            });
+            // eslint-disable-next-line no-console
+        }, console.error);
     }
 
-    const {
-      animal,
-      breed,
-      location,
-      description,
-      name,
-      media,
-      showModal,
-    } = this.state;
+    toggleModal = () => this.setState({ showModal: !this.state.showModal });
+    adopt = () => navigate(this.state.url);
+    render() {
+        if (this.state.loading) {
+            return <h1>loading ...</h1>;
+        }
 
-    return (
-      <div className="details">
-        <Carousel media={media} />
-        <div>
-          <h1>{name}</h1>
-          <h2>{`${animal} - ${breed} - ${location}`}</h2>
-          <ThemeContext.Consumer>
-            {([theme]) => (
-              <button
-                onClick={this.toggleModal}
-                style={{ backgroundColor: theme }}
-              >
-                Adopt {name}
-              </button>
-            )}
-          </ThemeContext.Consumer>
+        const {
+            animal,
+            breed,
+            location,
+            description,
+            name,
+            media,
+            showModal,
+        } = this.state;
 
-          <p>{description}</p>
-          {showModal ? (
-            <Modal>
-              <h1>Would you like to adopt {name}?</h1>
-              <div className="buttons">
-                <button onClick={this.adopt}>Yes</button>
-                <button onClick={this.toggleModal}>No, I am a monster</button>
-              </div>
-            </Modal>
-          ) : null}
-        </div>
-      </div>
-    );
-  }
+        return (
+            <div className="details">
+                <Carousel media={media} />
+                <div>
+                    <h1>{name}</h1>
+                    <h2>{`${animal} - ${breed} - ${location}`}</h2>
+                    <ThemeContext.Consumer>
+                        {([theme]) => (
+                            <button
+                                onClick={this.toggleModal}
+                                style={{ backgroundColor: theme }}
+                            >
+                                Adopt {name}
+                            </button>
+                        )}
+                    </ThemeContext.Consumer>
+
+                    <p>{description}</p>
+                    {showModal ? (
+                        <Modal>
+                            <h1>Would you like to adopt {name}?</h1>
+                            <div className="buttons">
+                                <button onClick={this.adopt}>Yes</button>
+                                <button onClick={this.toggleModal}>
+                                    No, I am a monster
+                                </button>
+                            </div>
+                        </Modal>
+                    ) : null}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default function DetailsWithErrorBoundary(props) {
-  return (
-    <ErrorBoundary>
-      <Details {...props} />
-    </ErrorBoundary>
-  );
+    return (
+        <ErrorBoundary>
+            <Details {...props} />
+        </ErrorBoundary>
+    );
 }
